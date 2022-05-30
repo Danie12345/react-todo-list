69ee84520d2d0b5ac47dab953199fbf82f1aa9c9
Daniel Malo <danielmaloosorio@gmail.com>
2022-05-30 14:46:42 -0500

    Fix TodosList eslint errors

--- src/functionBased/components/TodosList.js
+++ src/functionBased/components/TodosList.js
@@ -1,17 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import TodoItem from './TodoItem';
 
-const TodosList = (props) => (
-  <ul>
-    {props.todos.map((todo) => (
-      <TodoItem
-        key={todo.id}
-        todo={todo}
-        handleChangeProps={props.handleChangeProps}
-        deleteTodoProps={props.deleteTodoProps}
-        setUpdate={props.setUpdate}
-      />
-    ))}
-  </ul>
-);
+const TodosList = (props) => {
+  const {
+    handleChangeProps, deleteTodoProps, setUpdate, todos,
+  } = props;
+
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          handleChangeProps={handleChangeProps}
+          deleteTodoProps={deleteTodoProps}
+          setUpdate={setUpdate}
+        />
+      ))}
+    </ul>
+  );
+};
+
+TodosList.propTypes = {
+  handleChangeProps: PropTypes.func.isRequired,
+  deleteTodoProps: PropTypes.func.isRequired,
+  setUpdate: PropTypes.func.isRequired,
+  todos: PropTypes.PropTypes.objectOf(PropTypes.shape()).isRequired,
+};
+
 export default TodosList;

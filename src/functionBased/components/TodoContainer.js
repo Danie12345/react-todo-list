2136610df8a1d8e519484dc894bf2b8333b396a5
Daniel Malo <danielmaloosorio@gmail.com>
2022-05-30 13:00:38 -0500

    Add routes

--- src/functionBased/components/TodoContainer.js
+++ src/functionBased/components/TodoContainer.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react"
+
 import Header from "./Header"
 import InputTodo from "./InputTodo"
 import TodosList from "./TodosList"
+import About from "../pages/About"
+import NotMatch from "../pages/NotMatch"
+import Navbar from "./Navbar"
+
 import { v4 as uuidv4 } from "uuid"
-import { Route, Routes, Switch } from "react-router-dom"
+import { Route, Switch } from "react-router-dom"
 
 const TodoContainer = () => {
   const [todos, setTodos] = useState(getInitialTodos())
@@ -65,20 +70,29 @@ const TodoContainer = () => {
 
   return (
     <>
-      <Route exact path="/">
-        <div className="container">
-          <div className="inner">
-            <Header />
-            <InputTodo addTodoProps={addTodoItem} />
-            <TodosList
-              todos={todos}
-              handleChangeProps={handleChange}
-              deleteTodoProps={delTodo}
-              setUpdate={setUpdate}
-            />
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <div className="container">
+            <div className="inner">
+              <Header />
+              <InputTodo addTodoProps={addTodoItem} />
+              <TodosList
+                todos={todos}
+                handleChangeProps={handleChange}
+                deleteTodoProps={delTodo}
+                setUpdate={setUpdate}
+              />
+            </div>
           </div>
-        </div>
-      </Route>
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="*">
+          <NotMatch />
+        </Route>
+      </Switch>
     </>
   )
 }

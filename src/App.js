f6fc7d618f8fb8549ebefa235b671909bf3917d2
Daniel Malo <danielmaloosorio@gmail.com>
2022-05-30 13:46:53 -0500

    Update routes fix

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import './App.css';
 import React from 'react';
 import TodoContainer from "./functionBased/components/TodoContainer"
 import "./functionBased/App.css"
-import { HashRouter as Router } from "react-router-dom"
+import { BrowserRouter as Router } from "react-router-dom"
 
 function App() {
   return (
     <React.StrictMode>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <TodoContainer />
       </Router>
     </React.StrictMode>
